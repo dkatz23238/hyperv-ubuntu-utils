13a6cfd0b6f583f9ed323f2d35acec0e3c906fe7
David Emmanuel Katz <37519472+dkatz23238@users.noreply.github.com>
2018-12-21 22:01:58 -0300

    Update update-cloud-tools.sh

--- update-cloud-tools.sh
+++ update-cloud-tools.sh
@@ -1,1 +1,1 @@
-sudo apt-get update && sudo apt-get install hv-kvp-daemon-init linux-tools-$(uname -r) linux-cloud-tools-$(uname -r) -y
+sudo apt-get update && sudo apt-get install linux-azure linux-tools-$(uname -r) linux-cloud-tools-$(uname -r) -y
